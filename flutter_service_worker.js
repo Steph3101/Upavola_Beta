9e90c72fe538766150f7480ad4e0640060d00224
Stéphane Azzopardi <contact@stephane-azzopardi.fr>
2024-02-22 16:18:35 +0100

    Remove parse debug mode

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "40db803c4e2e8e7dafcb2c554a57ff00",
-"index.html": "fc68ccfe1dba24cd28868e19e22b2a60",
-"/": "fc68ccfe1dba24cd28868e19e22b2a60",
-"main.dart.js": "098204bed0670ddb384483c36d251eb0",
+"index.html": "862dead65c093adc99a53266ac1ac297",
+"/": "862dead65c093adc99a53266ac1ac297",
+"main.dart.js": "e87ff5b599807b3af354145258d8bbb4",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
